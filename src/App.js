1143ff06d3f3382122ba821789cd8451beda05a1
amirabotaref95 <amir.abotaref@runnableweb.com>
2022-12-24 18:03:56 +0200

    adding new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import "./App.css";
-import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
+
+import { Button, TextField, Box, Container } from "@mui/material";
+
+import { io } from "socket.io-client";
+
+// import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  const [socket, setSocket] = useState(null);
+  const [code, setCode] = useState("");
+
+  useEffect(() => {
+    setSocket(io("http://localhost:4000"));
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on("code-from-server", (data) => {
+      console.log("code recieved", data);
+    });
+  }, [socket]);
+
+  function handleForm(e) {
+    e.preventDefault();
+    socket.emit("send-code", { code });
+    setCode("");
+  }
+
+  return (
+    <div>
+      <Box component="form" onSubmit={handleForm}>
+        <TextField
+          size="small"
+          label="Write your Code"
+          variant="outlined"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <Button variant="text" type="submit">
+          send
+        </Button>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
